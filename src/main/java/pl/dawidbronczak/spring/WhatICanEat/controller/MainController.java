ee6994bdf6c6279e70523d3bb5a0ed5ba71f622c
czakos93 <dawidbronczak1993@gmail.com>
2018-08-28 08:32:07 +0200

    implement jquery autocomplete

---
+++ src/main/java/pl/dawidbronczak/spring/WhatICanEat/controller/MainController.java
@@ -0,0 +1,14 @@
+package pl.dawidbronczak.spring.WhatICanEat.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+	
+	@GetMapping("/")
+	public String getMainPage() {
+		return "main";
+	}
+
+}
